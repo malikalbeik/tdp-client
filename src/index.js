d0b21f95e6a5286794257a6ca4501ab26442c782
Malik Albeik <abd.beik@me.com>
2019-06-27 21:16:31 +0300

    Add: added redux state's functions

--- src/index.js
+++ src/index.js
@@ -2,16 +2,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Redux
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
+
+// Local Storage
+import { loadState, saveState } from './localStorage';
 
 // Routing & Links
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Redux dev tools
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// Redux Store
+const persistedState = loadState();
+const store = createStore(rootReducer, persistedState, reduxDevTools);
+
+store.subscribe(_ => {
+    saveState({
+        ...store.getState(),
+        theme: store.getState().theme
+    });
+});
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, document.getElementById('root')
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/reducers/index.js
@@ -0,0 +1,78 @@
+// Redux
+import { combineReducers } from 'redux'
+
+// Action types
+import * as types from '../actions/types';
+
+// Helpers
+import { objectFromArray } from '../utils';
+
+/**
+ * Theme Reducers
+ */
+function theme(state = {}, action) {
+    const { theme } = action;
+
+    switch (action.type) {
+        // load theme to store
+        case types.LOAD_THEME:
+            return {
+                ...theme
+            };
+        // any other action: return all posts
+        default:
+            return state;
+    }
+}
+
+/**
+ * Blog Posts Reducers
+ */
+function blogPosts(state = {}, action) {
+  const {posts, post} = action;
+
+  switch (action.type) {
+    // load posts to store
+    case types.LOAD_BLOG_POSTS:
+      return {
+        ...objectFromArray(posts)
+      };
+    // add a post
+    case types.ADD_BLOG_POST:
+      return {
+        ...state,
+        [post.id]: post
+      };
+    // any other action: return all posts
+    default:
+      return state;
+  }
+}
+
+/**
+ * Contents Reducers
+ */
+function contents(state = {}, action) {
+    const { contents, content } = action;
+
+    switch (action.type) {
+        // load projectss to store
+        case types.LOAD_CONTENETS:
+            return {
+                ...state,
+                ...objectFromArray(contents)
+            };
+        // add a content
+        case types.ADD_CONTENT:
+            return {
+                ...state,
+                [content.slug]: content
+            };
+        // any other action: return all projectss
+        default:
+            return state;
+    }
+}
+
+// export all above reducers combined
+export default combineReducers({ theme, blogPosts, contents });
