288116eca9159604b51b3e0ce10c134e48553daf
Malik Albeik <abd.beik@me.com>
2019-06-27 21:17:33 +0300

    Add: added redux actions

---
+++ src/actions/types.js
@@ -0,0 +1,11 @@
+/**
+ * Action Types
+ */
+
+export const LOAD_THEME = 'LOAD_THEME';
+
+export const LOAD_BLOG_POSTS = 'LOAD_BLOG_POSTS';
+export const ADD_BLOG_POST = 'ADD_BLOG_POST';
+
+export const LOAD_CONTENETS = 'LOAD_CONTENETS';
+export const ADD_CONTENT = 'ADD_CONTENT';
