dadc887fae4ae7749fcb689fdf12e1d063403516
Malik Albeik <abd.beik@me.com>
2019-07-04 23:05:36 +0300

    Add: added initial navbar structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react';
+// React
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+// Redux
+import { connect } from 'react-redux';
+
+// Styled Components
+import styled, { ThemeProvider } from "styled-components";
+
+// Components
+import WebsiteWrapper from './components/WebsiteWrapper';
+import FlexWrapper from './components/FlexWrapper';
+import Navbar from './components/Navbar';
+
+// Routing & Links
+import { withRouter } from 'react-router-dom';
+
+// Themes
+import { getTheme } from './thems';
+
+class App extends Component {
+  render() {
+    const info = this.props.theme;
+    const theme = getTheme(info);
+    document.body.style.backgroundColor = theme.colors.background;
+
+    return (
+      <ThemeProvider theme={theme}>
+        <WebsiteWrapper>
+          <FlexWrapper>
+            <Navbar />
+          </FlexWrapper>
+        </WebsiteWrapper>
+      </ThemeProvider>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(theme) {
+  return theme;
+}
+
+export default withRouter(connect(mapStateToProps, null)(App));
