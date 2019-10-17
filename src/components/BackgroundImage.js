4b45909bdef1397898a9163b4c72c6e15bcb094a
Malik Albeik <abd.beik@me.com>
2019-10-17 04:15:08 +0300

    Edit: changed background styling

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -11,7 +11,6 @@ class BackgroundImage extends Component {
         return (
             <Wrapper>
                 <Image src={Background} alt="Logo"/>
-                <OverLay></OverLay>
             </Wrapper>
         );
     }
@@ -30,21 +29,6 @@ const Image = styled.img`
   min-width: 100%;
   z-index: 0;
   top: -100px;
-`;
-
-const OverLay = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0; 
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0,0,0,0.5);
-  z-index: 2;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
 
 export default BackgroundImage;
